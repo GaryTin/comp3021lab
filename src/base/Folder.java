acf14f462ead4940b0c8393f32d512fab637dec7
GaryTin <tinkwaiki@gmail.com>
2022-03-04 10:48:48 +0800

    Lab3

--- src/base/Folder.java
+++ src/base/Folder.java
@@ -1,8 +1,10 @@
 package base;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 
-public class Folder {
+public class Folder implements Comparable<Folder>{
 	private ArrayList<Note> notes;
 	private String name;
 
@@ -62,5 +64,88 @@ public class Folder {
 		
 		return name + ":" + nText + ":" + nImage;
 
+	}
+	
+	public void sortNotes()
+	{
+		Collections.sort(notes);
+	}
+	
+	@Override
+	public int compareTo(Folder o)
+	{
+		if (this.name.compareTo(o.name) >0  )
+		{
+			return 1;
+		}
+		else if(this.name.compareTo(o.name) < 0)
+		{
+			return -1;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+	
+	public List<Note> searchNotes(String keywords)
+	{
+		List<Note> result = new ArrayList<Note>();
+		keywords = keywords.toUpperCase();
+		String[] temp_keyword_list = keywords.split(" ");
+		int index = 0;
+		List<String> keyword_list = new ArrayList<String>();
+		while (index < temp_keyword_list.length)
+		{
+			if (index < temp_keyword_list.length-2 && temp_keyword_list[index+1].equals("OR"))
+			{
+				keyword_list.add("o "+temp_keyword_list[index]+" "+temp_keyword_list[index+2]);
+				index = index +3;
+			}
+			else
+			{
+				keyword_list.add(temp_keyword_list[index]);
+				index = index +1;
+			}
+		}
+		//System.out.println(keyword_list);
+		for (Note note:notes)
+		{
+			//System.out.println(note.getTitle()+"\t"+note.getContent());
+			boolean valid = true;
+			for (String keyword:keyword_list)
+			{
+				if (keyword.substring(0,1).equals("o"))
+                {
+                    String[] sub_keyword_list = keyword.split(" ");
+                    //for (String t:sub_keyword_list)
+                    //	System.out.println(t);
+                    if (!(note.getTitle().toUpperCase().contains(sub_keyword_list[1]) || note.getTitle().toUpperCase().contains(sub_keyword_list[2]) || (note.getContent()!=null && (note.getContent().toUpperCase().contains(sub_keyword_list[1]) || note.getContent().toUpperCase().contains(sub_keyword_list[2])))) )
+                    {
+                    	valid = false;
+                    	break;
+                    }
+                    
+                }
+                else
+                {
+                    if (!(note.getTitle().toUpperCase().contains(keyword)||(note.getContent()!=null && note.getContent().toUpperCase().contains(keyword))) )
+                    {
+                    	valid = false;
+                    	break;
+                    }
+                }
+			}
+			//System.out.println(valid);
+			if (valid)
+			{
+				result.add(note);
+			}
+				
+		}
+		
+		
+		return result;
+		
 	}
 }
--- src/base/Note.java
+++ src/base/Note.java
@@ -2,7 +2,7 @@ package base;
 import java.util.Date;
 import java.util.Objects;
 
-public class Note {
+public class Note implements Comparable<Note>{
 	
 	private Date date;
 	private String title;
@@ -27,6 +27,34 @@ public class Note {
 			return false;
 		Note other = (Note) obj;
 		return Objects.equals(title, other.title);
+	}
+	
+	@Override
+	public int compareTo(Note o)
+	{
+		if (this.date.compareTo(o.date) >0  )
+		{
+			return 1;
+		}
+		else if(this.date.compareTo(o.date) < 0)
+		{
+			return -1;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+	
+	
+	public String toString()
+	{
+		return date.toString()+"\t"+title;
+	}
+	
+	public String getContent()
+	{
+		return null;
 	}
 
 
