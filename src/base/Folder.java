70327d8195d8927e8e1aa472526ff55a100d3187
GaryTin <tinkwaiki@gmail.com>
2022-03-10 19:56:42 +0800

    LAB4

--- src/base/Folder.java
+++ src/base/Folder.java
@@ -3,8 +3,9 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.io.Serializable;
 
-public class Folder implements Comparable<Folder>{
+public class Folder implements Comparable<Folder>,Serializable{
 	private ArrayList<Note> notes;
 	private String name;
 
--- src/base/TextNote.java
+++ src/base/TextNote.java
@@ -1,6 +1,17 @@
 package base;
 
-public class TextNote extends Note{
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+
+
+public class TextNote extends Note {
 
 	private String content;
 	
@@ -15,4 +26,65 @@ public class TextNote extends Note{
 	{
 		return content;
 	}
+	
+	public TextNote(File f) {
+		super(f.getName());
+		this.content = getTextFromFile(f.getAbsolutePath());
+	}
+	
+	private String getTextFromFile(String absolutePath) {
+		String result = "";
+		FileInputStream fis = null;
+		InputStreamReader in = null;
+	    try 
+	    {
+            fis = new FileInputStream(absolutePath);
+            in = new InputStreamReader(fis);
+            int i;
+            while ((i =in.read())!=-1)
+            {
+            	result += (char)i;
+            }
+            in.close();
+	    } 
+	    catch (FileNotFoundException e)
+	    {
+	    	System.out.println("File ["+ absolutePath +"] not found");
+	    }
+	    catch (Exception e) 
+	    {
+	    	System.out.println("Error");
+	        e.printStackTrace();
+	    }
+
+		return result;
+	}
+	public void exportTextToFile(String pathFolder) 
+	{
+		
+		FileWriter f1 = null;
+		BufferedWriter output = null;
+		if (pathFolder =="")
+		{
+			pathFolder=".";
+		}
+		File file = new File( pathFolder + File.separator + this.getTitle().replaceAll(" ","_") + ".txt");
+		//System.out.println("=============");
+		//System.out.println(file.getAbsolutePath());
+		//System.out.println("=============");
+		try
+		{
+			f1 = new FileWriter(file);
+			output = new BufferedWriter(f1);
+			output.write(this.content);
+			//System.out.println(this.content);
+			output.close();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+
+	}
+
 }
